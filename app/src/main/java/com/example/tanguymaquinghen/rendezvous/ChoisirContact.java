70a6961a2aa1cd38e50b02d24aa21469bf62812d
Tanguy Maquinghen <tanguy.maquinghen@gmail.com>
2018-10-25 12:32:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tanguymaquinghen/rendezvous/ChoisirContact.java
@@ -0,0 +1,13 @@
+package com.example.tanguymaquinghen.rendezvous;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoisirContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choisir_contact);
+    }
+}
